0e412c0a9eddacbcb9576b9d47cfc5b5397cb375
pixelsyndicate <wdobson@pixelsyndicate.com>
2015-06-19 14:56:54 -0500

    code cleanup
    removing clunky bootstrap elements, long text

--- GuideBook/Guidebook.js
+++ GuideBook/Guidebook.js
@@ -6,18 +6,18 @@ var guidebookConfig = function ($routeProvider) {
         controller: 'ChaptersController',
         templateUrl: 'view/chapters.html'
     }).when('/chapter/:chapterId', {
-            controller: 'ChaptersController',
-            templateUrl: 'view/chapters.html'
-        }).when('/addNote/:chapterId', {
-            controller: 'AddNoteController',
-            templateUrl: 'view/AddNote.html'
-        }).when('/deleteNote/:chapterId/:noteId', {
-            controller: 'DeleteNoteController',
-            templateUrl: 'view/addNote.html'
-        });
+        controller: 'ChaptersController',
+        templateUrl: 'view/chapters.html'
+    }).when('/addNote/:chapterId', {
+        controller: 'AddNoteController',
+        templateUrl: 'view/AddNote.html'
+    }).when('/deleteNote/:chapterId/:noteId', {
+        controller: 'DeleteNoteController',
+        templateUrl: 'view/addNote.html'
+    });
 };
 
 // here we are defining an Angular namespace ('called a module')
 var Guidebook = angular.module('Guidebook', ['ngRoute'])
-        .config(guidebookConfig);
+    .config(guidebookConfig);
 // using a namespace, we are keeping the controllers and models in their own application namespace
